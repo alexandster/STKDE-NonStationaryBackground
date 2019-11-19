807125e82d1063c939af0faa7c64082215dce389
Alexander Hohl <34194710+alexandster@users.noreply.github.com>
2019-11-19 14:01:04 -0700

    code update

--- histo.r
+++ histo.r
@@ -1,5 +1,5 @@
 #histo.r
 #this script creates a histogram of the differences between upper and lower envelope.
 
-inTable <- read.table("outputs/envelope_diff_v2.txt", header = FALSE, sep = ",", dec = ".")
+inTable <- read.table("outputs/envelope/envelope_diff.txt", header = FALSE, sep = ",", dec = ".")
 histo <- hist(inTable$V4)
