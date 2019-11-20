2449df101e97031242a32a1d1183ed53c92721fc
Alexander Hohl <34194710+alexandster@users.noreply.github.com>
2019-11-19 22:22:35 -0700

    Update envelope_diff.py

--- envelope_diff.py
+++ envelope_diff.py
@@ -6,16 +6,8 @@ wDir = 'outputs/envelope'
 envLo = np.loadtxt(wDir + os.sep + 'envelope_lower.txt', delimiter=",")
 envUp = np.loadtxt(wDir + os.sep + 'envelope_upper.txt', delimiter=",")
 
-print("Arrays read")
-
 envDiff = envUp[:,3] - envLo[:,3]
-
-print("Difference calculated")
 
 finalArr = np.column_stack((envLo[:,:3],envDiff))
 
-print("Arrays stacked")
-
 np.savetxt(wDir + os.sep + 'envelope_diff.txt', finalArr, delimiter=',')
-
-print("done")
