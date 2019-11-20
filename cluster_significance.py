be7a08d281adb334cb7f57136a06f5e262d3b7d3
Alexander Hohl <34194710+alexandster@users.noreply.github.com>
2019-11-19 21:15:10 -0700

    ef

--- cluster_significance.py
+++ cluster_significance.py
@@ -7,30 +7,30 @@ import math, sys, os
 
 #compute p value of observed odds ratio
 percentile = 95       # percentile threshold parameter
-neighThres = 40       # minsup: minimum number of ST neighbors threshold
+neighThres = 45       # minsup: minimum number of ST neighbors threshold
 
 #since we determined that the uncertainty that stems from population simulatino is negliglible, I picked simulation run 0 as the observed population.
 #this allowed me to simulate the cases for significance testing.
 
 #load index array: list of index numbers that denote voxles part of cluster in indatArr observed -- THIS IS THE OBSERVED CLUSTER (95 percentile)
-indexArr = np.load('outputs/ST_IB_4/clustIndex_' + str(neighthres) + '_' + str(percentile) + ".npy")
+indexArr = np.load('outputs/ST_IB_4/sim_0/clustIndex_' + str(neighThres) + '_' + str(percentile) + ".npy")
 
 #load observed odds ratio (at 95 percentile)
-obsOddsRatio = np.loadtxt('outputs/ST_IB_4/odds_ratio_" + str(neighThres) + ".txt")[5][-1]
+obsOddsRatio = np.loadtxt('outputs/ST_IB_4/sim_0/odds_ratio_' + str(neighThres) + ".txt", delimiter = ',', usecols=-1)[5]
 
 #open output file
 outDir = 'outputs/cluster'
 if not os.path.exists(outDir):
     os.makedirs(outDir)
 
-outFile = open(outDir + 'cluster_significance_' + str(neighThres) + "_" + str(percentile) + ".txt", "w")
+outFile = open(outDir + os.sep + 'cluster_significance_' + str(neighThres) + "_" + str(percentile) + ".txt", "w")
 
 outFile.write('Observed odds ratio: ' + str(obsOddsRatio) + "\n")
 
 #odds ratios from simulated clusters
 simOddsRatio = 0
 i = 0
-while i <= 99:
+while i <= 1:
 
     #load regular grid: 2d grid of tuples: (x, y, t, nCount, pCount, k) --- simulated density
     indatArr = np.loadtxt('outputs/ST_MC_3/sim_' + str(i) + os.sep + "density_" + str(neighThres) + ".txt", delimiter=",")
@@ -51,7 +51,7 @@ while i <= 99:
     nonClustRatio = casNonClust/conNonClust
     oddsRatio = clustRatio/nonClustRatio
 
-    outFile.write('Odds ratio simulation ' + str(i)) + ' :' + str(oddsRatio) + "\n")
+    outFile.write('Odds ratio simulation ' + str(i) + ' :' + str(oddsRatio) + "\n")
 
     i += 1
 
